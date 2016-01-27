d9da3129a0897851011c861e60a32f545e6094ec
Jackson Cafazzo and Jessica Fix <doctorjuno@gmail.com>
2016-01-27 12:07:54 -0800

    Add row and begin jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,18 @@ function randomWordGenerator (wordArray) {
 function getColumnWidths (currentArray) {
   return 12 / currentArray.length;
 }
+
+$(document).ready(function() {
+  var wordBank = ["mango", "guava", "hibiscus", "megabat", "nectar", "passionfruit", "watermelon", "bananas", "seeds", "peel", "pulp"]
+  $("body").data("wordBank", wordBank);
+
+  $("button#playNewGame").submit(function(event) {
+    var wordBank = $("body").data("wordBank");
+    var randomWord = randomWordGenerator(wordBank);
+    var colWidths = getColumnWidths(randomWord);
+    for (var i = 0; i < randomWord.length; i++) {
+      $("#spacesRow").append('<div class="col-md-' + colWidths + '"></div>')
+    }
+    event.preventDefault();
+  )};
+)};
