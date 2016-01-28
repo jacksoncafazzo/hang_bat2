985d1e2bd67e9030286a5897c275feddc08ca20f
Jackson Cafazzo and Jessica Fix <jcfix.pdx@gmail.com>
2016-01-27 17:01:41 -0800

    final commit before end of day

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,26 @@ $(document).ready(function() {
     var wordBank = $("body").data("wordBank");
     var randomWord = randomWordGenerator(wordBank);
     for (var i = 0; i < randomWord.length; i++) {
-      $("#spacesRow").append('<div class="col-sm-' + getColumnWidths(randomWord) + ' letterSpaces" id="letterSpaces' + i + '"></div>');
+      $("#spacesRow").append('<div class="col-sm-' + getColumnWidths(randomWord) + ' letterSpaces" id="letterSpaces' + i + '"><div class="userGuess" id="userGuess' + i + '"></div></div>');
     }
     for (var i = 0; i < randomWord.length; i++) {
       $("#letterSpaces" + i).text(randomWord[i]);
 
     }
+    for (var i = 0; i < randomWord.length; i++) {
+      $("#userGuess" + i).hide();
+    }
   });
 });
+
+<div class="col-sm-3 letterSpaces" id="letterSpaces0">
+  <div class="userGuess" id="userGuess0">
+  </div>
+</div>
+<div class="col-sm-3 letterSpaces" id="letterSpacesi">
+  <div class="userGuess" id="userGuessi">
+  </div>
+</div><div class="col-sm-3 letterSpaces" id="letterSpacesi">
+  <div class="userGuess" id="userGuessi">
+  </div>
+</div>
